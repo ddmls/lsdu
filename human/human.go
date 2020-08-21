4d9dc08d9b9cc9e58831c12258eee1af27ffebb3
Dimos Dimoulis <58330181+ddmls@users.noreply.github.com>
2020-08-21 14:55:05 +0300

    Fractional sizes

--- human/human.go
+++ human/human.go
@@ -1,6 +1,8 @@
 package human
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const minInt64 = -9223372036854775808
 const maxInt64 = 9223372036854775807
@@ -15,14 +17,20 @@ const (
 )
 
 // Humanize reports the size in power of 1024 units (KiB, MiB etc)
+// Use width 6
 func Humanize(size int64) string {
-	suffixes := [...]string{"b", "K", "M", "G", "T", "P"}
-	for base := 0; base < len(suffixes); base++ {
-		if size < 1024 {
-			return fmt.Sprintf("%d%s", size, suffixes[base])
+	suffixes := [...]string{"b", "K", "M", "G", "T", "P", "E"}
+	var unit int64 = KiB
+	var i int
+	for i = range suffixes {
+		if size < unit {
+			break
 		}
-		size >>= 10
+		unit <<= 10
 	}
-	// Too big
-	return fmt.Sprintf("%d%s", size, suffixes[len(suffixes)-1])
+	unit >>= 10
+	if size < 1024 {
+		return fmt.Sprintf("%d%s", size, suffixes[i])
+	}
+	return fmt.Sprintf("%.1f%s", float64(size)/float64(unit), suffixes[i])
 }
